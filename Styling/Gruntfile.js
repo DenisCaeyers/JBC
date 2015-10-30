08e9550d890ce2f8af708c13d2cf698b8d563c92
DenisCaeyers <denis.caeyers@cegeka.be>
2015-10-30 12:06:45 +0100

    Added compass & grunt

---
+++ Styling/Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+	compass:{
+		dev:{
+			options:{
+				sassDir: "sass",
+				cssDir: "stylesheets"
+			}
+		}
+	},
+	watch:{
+		css:{
+			files:'**/*.scss',
+			tasks: ['compass']
+		}
+	}  
+  })
+  
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  
+  grunt.registerTask('default', ['watch']);
+};
